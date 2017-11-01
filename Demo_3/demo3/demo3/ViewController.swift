f82d94de9b623610c5908f7b46826828ad0f60e7
Jessie Dowding <jessiedowding@Jessies-iMac.local>
2017-11-01 09:03:47 -0700

    demo 3 initial

---
+++ Demo_3/demo3/demo3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo3
+//
+//  Created by Jessie Dowding on 11/1/17.
+//  Copyright © 2017 cst495_JessieDowding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
