01105bfcda1d0fa400d583b02c5263a1679cbf8a
Jessie Dowding <dowd8366@m506-246-25987.local>
2017-12-11 09:07:07 -0800

    challenge 1

---
+++ Challenge1/challenge1/challenge1/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  challenge1
+//
+//  Created by Jessie Dowding on 12/11/17.
+//  Copyright © 2017 Jessie Dowding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var myImg: UIImageView!
+    
+    @IBOutlet var URL: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func openURL(_ sender: AnyObject) {
+        let path=URL.text!
+        let url = NSURL(string: path)
+        let data = NSData(contentsOf: url as! URL)
+        let img = UIImage(data: data as! Data)
+        myImg.image = img
+    }
+    
+    
+    @IBAction func takePhoto(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImg.contentMode = .scaleToFill
+            myImg.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func savePhoto(_ sender: AnyObject) {
+        let imageData = UIImagePNGRepresentation(myImg.image!)
+        let compresedImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compresedImage!, nil, nil, nil)
+        
+        let alert = UIAlertController(title: "Saved", message: "Your image has been saved", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    @IBAction func Rotate(_ sender: Any) {
+        self.myImg.transform = CGAffineTransform.init(rotationAngle: 60);
+        
+    }
+}
+
+
+
